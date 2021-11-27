aaa6fe85d5c1fc3140c3f6f6f16908c29a6a4bdb
gallelizabeth <liza.a.gall@gmail.com>
2021-11-27 13:39:08 +0300

    сделали рисование при движении

--- movingThings.py
+++ movingThings.py
@@ -3,30 +3,23 @@ import pygame
 SIZE = WIDTH, HEIGHT = 800, 600
 RADIUS = 20
 BLACK = 0, 0, 0
-RED = 255, 0, 0
-FPS = 60  # кадров в секунду
+BLUE = 0, 0, 255
 
 if __name__ == '__main__':
     pygame.init()
+    pygame.display.set_caption('Графический редактор v0.0.0.1')
     screen = pygame.display.set_mode(SIZE)
 
     running = True
-    x_pos = 0
-    v = 100  # пикселей в секунду
-    clock = pygame.time.Clock()
+    screen.fill(BLACK)
     while running:  # основной игровой цикл
         for event in pygame.event.get():
             # обработка всех событий
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEMOTION:
+                pygame.draw.circle(screen, BLUE, event.pos, RADIUS)
         # изменения, не зависящие от событий - действий пользователя или таймера
-        screen.fill(BLACK)
-        pygame.draw.circle(screen, RED, (x_pos, HEIGHT // 2), RADIUS)
-        x_pos += v * clock.tick(FPS) / 1000
-        # clock.tick() - время от начала работы программы в миллисекундах
-        # x = v * t (в секундах)
-        if x_pos > WIDTH or x_pos < 0:
-            v *= -1  # изменить направление
         pygame.display.flip()
 
     pygame.quit()
